16720ba9ed65604e0ab49e70fee3657d0833638e
AIPMLab <71798737+AIPMLab@users.noreply.github.com>
2024-06-21 12:54:28 +0800

    Add files via upload

---
+++ nets/models.py
@@ -0,0 +1,66 @@
+import torch.nn as nn
+import clip
+from utils.clip_util import freeze_param, get_image_features
+
+
+class ClipModelat(nn.Module):
+
+    CLIP_MODELS = [
+        'RN50',
+        'RN101',
+        'RN50x4',
+        'RN50x16',
+        'RN50x64',
+        'ViT-B/32',
+        'ViT-B/16',
+        'ViT-L/14',
+        'ViT-L/14@336px'
+    ]
+
+    def __init__(self, model_name='Vit-B/32', device='cuda', logger=None, attention=True, freezepy=True):
+        super(ClipModelat, self).__init__()
+        self.logger = logger
+        if type(model_name) is int:
+            model_name = self.index_to_model(model_name)
+        self.model, self.preprocess = clip.load(
+            model_name, device=device, jit=False)
+        self.model.eval()
+        self.model.to(device)
+        self.model_name = model_name
+        self.attention = attention
+        self.freezepy = freezepy
+        self.device = device
+
+    def initdgatal(self, dataloader):
+
+        for batch in dataloader:
+            image, _, label = batch
+            image = image.to(self.device)
+            label = label.to(self.device)
+            image_features = get_image_features(
+                image, self.model, self.preprocess)
+            break
+        if self.freezepy:
+            freeze_param(self.model)
+
+        if self.attention:
+            self.fea_attn = nn.Sequential(nn.Linear(image_features.shape[1], image_features.shape[1]), nn.BatchNorm1d(image_features.shape[1]), nn.LeakyReLU(
+            ), nn.Linear(image_features.shape[1], image_features.shape[1]), nn.Softmax(dim=1)).to(self.device)
+
+            # For FedClip
+            # self.fea_attn = nn.Sequential(nn.Linear(image_features.shape[1], image_features.shape[1]),
+            # nn.Tanh(), nn.Linear(image_features.shape[1], image_features.shape[1]), nn.Softmax(dim=1)).to(self.device)
+
+
+
+    def index_to_model(self, index):
+        return self.CLIP_MODELS[index]
+
+    @staticmethod
+    def get_model_name_by_index(index):
+        name = ClipModelat.CLIP_MODELS[index]
+        name = name.replace('/', '_')
+        return name
+
+    def setselflabel(self, labels):
+        self.labels = labels
